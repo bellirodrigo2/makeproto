959455cc5268f457836cc9294463c87ca7c04907
RBELLI <bellirodrigo2@gmail.com>
2025-07-02 08:21:09 -0300

    ...

--- makeproto/__init__.py
+++ makeproto/__init__.py
@@ -1,4 +1,4 @@
-__all__ = ["compile_service", "IService"]
+__all__ = ["compile_service", "IService", "ILabeledMethod"]
 
 from makeproto.build_service import compile_service
-from makeproto.interface import IService
+from makeproto.interface import IService, ILabeledMethod
