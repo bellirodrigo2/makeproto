217ea8f652c60854221236dc4f124b6c6df13c32
RBELLI <bellirodrigo2@gmail.com>
2025-07-08 16:30:59 -0300

    ...

--- makeproto/validators/custommethod.py
+++ makeproto/validators/custommethod.py
@@ -31,12 +31,12 @@ class CustomPass(CompilerPass):
         if self._finish is not None:
             self._finish(self)
 
-    def _report(
-        self, error_msg: List[str], name: str, service: ServiceTemplate
-    ) -> None:
-        report = self.ctx.get_report(block_name=service)
+    def _report(self, error_msg: List[str], method: MethodTemplate) -> None:
+        report = self.ctx.get_report(block_name=method.service)
         for error in error_msg:
-            report.report_error(CompileErrorCode.RUNTIME_POSSIBLE_ERROR, name, error)
+            report.report_error(
+                CompileErrorCode.RUNTIME_POSSIBLE_ERROR, method.name, error
+            )
 
     def visit_service(self, block: ServiceTemplate) -> None:
         for field in block.methods:
@@ -44,4 +44,4 @@ class CustomPass(CompilerPass):
 
     def visit_method(self, method: MethodTemplate) -> None:
         error_msg = self._visit_method(method.method_func)
-        self._report(error_msg, method.name, service=method.service)
+        self._report(error_msg, method)
